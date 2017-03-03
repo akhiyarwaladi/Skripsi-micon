bbeefc2b386ccd9199d2604daa81340b19027e84
akhiyarwaladi <akiyar18@gmail.com>
2017-03-03 13:42:04 +0700

    hari ini

--- main.cpp
+++ main.cpp
@@ -77,7 +77,7 @@ int main(){
 				printf("Tambahan  %f\n" , dur2);
 				int dur4 = dur1;
 				int dur5 = dur2;
-				serialPutchar (handle, dur4);
+				serialPutchar (handle, "A");
 				//cmd[0] = dur4;
 				//write(file, cmd, 1);
 				//cmd[0] = dur5;
