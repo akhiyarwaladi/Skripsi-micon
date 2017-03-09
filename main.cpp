f00eec683500018d0a58443ed06c37da4f99050b
akhiyarwaladi <akiyar18@gmail.com>
2017-03-09 09:27:17 +0700

    tambah

--- main.cpp
+++ main.cpp
@@ -45,8 +45,41 @@ int main(){
 			printf("Id Alat= %d\n", idalat);
 			printf("Data Received= %d\n", temp);
 			
-			uk1, uk2 = hitung(awal, temp);
-			if(uk2>0){
+			//uk1, uk2 = hitung(awal, temp);
+			int n = 2;
+			float Er[n], dEr[n];
+			float HPSp = 5.0;
+			float f1 = 0.83;
+			float f2 = 1.00;
+			float Um = 1.00;
+			float __Uk[n];
+			float HPc[n] = {awal, temp};
+			double dur1, dur2;
+
+			for (int i=0; i<n; i++){
+				Er[i] = HPc[i]-HPSp;
+				printf("Er %f\n" , Er[i]);
+			}
+			for (int i=0; i<n; i++){
+				if (i==0) dEr[i] = Er[i];
+				else if (i==10) dEr[10] = 0;
+				else dEr[i] = Er[i]-Er[i-1];
+				printf("dEr %f\n" , dEr[i]);
+			}
+			for (int i = 0; i < n; i++){
+				float _theta = theta(f1, Er[i], dEr[i]);
+				float _Dk = Dk(f1, Er[i], dEr[i]);
+				float _mN = mN(_theta);
+				float _mD = mD(_Dk, f2);
+				float _Uk = Uk(_mN, _mD, Um);
+
+				__Uk[i] = _Uk;
+			}
+			for (int i = 0; i < n; i++){
+				printf("Uk %f\n" , __Uk[i]);
+			}
+		
+			if(__Uk[1]>0){
 				double time = uk2;
 				double dur = time*1*60;
 				OpTime = dur;
