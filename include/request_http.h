f077c4432dc4d0407b91c96029b7f7e8d1cd2807
akhiyarwaladi <akiyar18@gmail.com>
2017-06-10 02:40:07 +0700

    lah

--- include/request_http.h
+++ include/request_http.h
@@ -1,7 +1,7 @@
 static const char *URL_TOSERVER= "curl -X POST -H \"Authorization: 5d55ed73dda2730ec3e01a5f8c631966\" -H \"Cache-Control: no-cache\" -H \"Postman-Token: c6054f6b-8f38-6630-f6da-1101ef4d3e59\" -H \"Content-Type: application/x-www-form-urlencoded\" -d \'hpsp=%f&hpc=%f&humid=%f&temp=%f&uk=%f&optime=%f&idalat=%f\' \"http://192.168.43.98/Sigap-Server/v1/data_sensor\"";
 static const char *URL_NOTIFICATION= "curl -X POST -H \"Cache-Control: no-cache\" -H \"Postman-Token: 26d74e27-95c1-ec91-5c25-d7e14db55344\" -H \"Content-Type: application/x-www-form-urlencoded\" -d \'to=%s&title=%s&message=%s\' \"http://192.168.43.98/Sigap-Server/v1/sendsingle\"";
 static const char *URL_UPDATEALAT= "curl -X PUT -H \"Authorization: 5d55ed73dda2730ec3e01a5f8c631966\" -H \"Cache-Control: no-cache\" -H \"Postman-Token: 0f4cf5df-09ec-d9e4-2642-60ca3c950289\" -H \"Content-Type: application/x-www-form-urlencoded\" -d \'rssi=%f&battery=%f&idalat=%f&status=%f\' \"http://192.168.43.98/Sigap-Server/v1/alatuser\"";
-static const char *BASE_URL = "http://192.168.0.113:3000/api";
+static const char *BASE_URL = "http://192.168.43.98:3000/api";
 void sendDataToServer(double hpsp, double hpc, double humid, double temp, double uk, double opt, double idalat){
 	char str[1000];
 	sprintf(str, URL_TOSERVER, hpsp, hpc, humid, temp, uk, opt, idalat);
